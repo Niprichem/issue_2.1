7af3d6398b6aa334c1ccb2ec6a0651a8396eebd2
Niprichem <vbolshakoff@yandex.ru>
2017-11-03 01:01:19 +0600

    Change code.

--- Dict.py
+++ Dict.py
@@ -1,22 +1,49 @@
-# -*- coding: utf8 -*-
-from pprint import pprint
+cook_book = {}
+
+def get_shop_list_by_dishes(dishes, person_count):
+    shop_list = {}
+    for dish in dishes:
+        for ingridient in cook_book[dish]:
+            new_shop_list_item = dict(ingridient)
+            new_shop_list_item['quantity'] *= person_count
+            if new_shop_list_item['ingridient_name'] not in shop_list:
+                shop_list[new_shop_list_item['ingridient_name']] = new_shop_list_item
+            else:
+                shop_list[new_shop_list_item['ingridient_name']]['quantity'] += new_shop_list_item['quantity']
+    return shop_list
+
+
+def print_shop_list(shop_list):
+    # for shop_list_item in shop_list.values():
+    #   print('{ingridient_name} {quantity} {measure}'.format(**shop_list_item))
+    for shop_list_item in shop_list.values():
+        print(
+            '{} {} {}'.format(shop_list_item['ingridient_name'], shop_list_item['quantity'], shop_list_item['measure']))
+
+
+def create_shop_list():
+    person_count = int(input('Введите количество человек'))
+    dishes = input('Введите блюда в расчете на одного человека через запятую').lower().split(',')
+    shop_list = get_shop_list_by_dishes(dishes, person_count)
+    print_shop_list(shop_list)
 
 
 def main():
-    with open('E:/Python/Задание_2.1/Menu.txt', encoding='utf-8-sig') as f:
-        cook_book = {}
+    with open('Menu.txt', encoding='utf-8-sig') as f:
+        cook_book_ = {}
         for line in f:
             name_dish = line.strip()
-            cook_book[name_dish] = [] #заполняем список ключами
+            cook_book_[name_dish] = [] #заполняем список ключами
             for _ in range(int(f.readline().strip())):
                 ingridient_name, quantity, measure = f.readline().strip().split(' | ')
-                cook_book[name_dish].append({
+                cook_book_[name_dish].append({
                     'ingridient_name': ingridient_name,
-                    'quantity': quantity,
+                    'quantity': int(quantity),
                     'measure': measure
                 })
-    print('cook_book = ')
-    pprint(cook_book)
+    global cook_book
+    cook_book = cook_book_
+    create_shop_list()
 
 if __name__ == '__main__':
     main()
